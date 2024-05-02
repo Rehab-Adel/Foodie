11f71282e3fdb1dc6fc3929c49849de3630d02ee
Rehab Adel <rehabadel1212@gmail.com>
2024-05-03 00:13:07 +0300

    add account controller, startup class and connection string in appsetting.json file

--- Foodies/Controllers/AccountController.cs
+++ Foodies/Controllers/AccountController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Foodies.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Foodies.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ApplicationDBContext _DBContext;
+        public AccountController(ApplicationDBContext dbContext)
+        {
+            _DBContext = dbContext;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,11 +21,47 @@ namespace Foodies.Controllers
         {
             return View();
         }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Login(string email, string password)
+        {
+            if(!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+            {
+                var user = _DBContext.Customers.FirstOrDefault(u => u.Email == email && u.Password == password);
+                if(user != null)
+                {
+                    RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Invalid email or Password");
+            return View();
+        }
         [HttpGet]
-
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                if(!_DBContext.Admins.Any())
+                {
+                    ModelState.AddModelError(string.Empty, "You can't register");
+                    return View(customer);
+                }
+
+
+                var existingUser = _DBContext.Customers.FirstOrDefault(u => u.Email == customer.Email);
+                if(existingUser == null)
+                { 
+                    _DBContext.Customers.Add(customer);
+                    _DBContext.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "This email is already existed");
+            }
+            return View(customer);
         }
     }
 }
