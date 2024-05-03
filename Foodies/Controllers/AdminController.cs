a7f6f26710b42dd643b6243247f80a78961e307c
Rehab Adel <rehabadel1212@gmail.com>
2024-05-03 05:38:55 +0300

    add admin controller and upload database backup

--- Foodies/Controllers/AdminController.cs
+++ Foodies/Controllers/AdminController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using Foodies.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 namespace Foodies.Controllers
 {
 	public class AdminController : Controller
 	{
+		private readonly ApplicationDBContext _DBContetx;
+		public AdminController(ApplicationDBContext dbContetx)
+		{
+			_DBContetx = dbContetx;
+		}
 		public IActionResult Index()
 		{
 			return View();
 		}
-	}
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var admin = _DBContetx.Admins.FirstOrDefault(a => a.Email == email && a.Password == password);
+                if (admin != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid email or password");
+                }
+            }
+            return View();
+        }
+    }
 }
